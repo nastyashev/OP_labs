e9c37fafe794f425a5108256f485db60ce751934
Анастасія Шевцова <nastya.sova2004@gmail.com>
2022-05-26 11:16:08 +0300

    fixed

--- lab_5/lab_5_py/lab_5_py/TCylinder.py
+++ lab_5/lab_5_py/lab_5_py/TCylinder.py
@@ -11,7 +11,7 @@ class TCylinder(TFigure):
     def find_surface_area(self):
         side_surface_area = 2 * self._Pi * self.__radius * self._height
         base_area = self._Pi * pow(self.__radius, 2)
-        return side_surface_area + base_area
+        return side_surface_area + 2 * base_area
 
     def find_volume(self):
         return self._Pi * pow(self.__radius, 2) * self._height
